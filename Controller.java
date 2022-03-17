95a8deb189e39da91bae97b365e5d7f92d6dd9fe
Jaret Wrintmore <jaret.wrintmore@gmail.com>
2022-03-17 11:21:06 +0000

    main functions added and taking input

---
+++ Controller.java
@@ -0,0 +1,9 @@
+public class Controller {
+
+  public static void main(String[] args) {
+    final int cport = Integer.parseInt(args[0]);
+    int R = Integer.valueOf(args[1]);
+    int timeout = Integer.valueOf(args[2]);
+    int rebalancePeriod = Integer.valueOf(args[3]);
+  }
+}
---
+++ Dstore.java
@@ -0,0 +1,11 @@
+
+public class Dstore {
+
+  public static void main(String[] args) {
+    final int port = Integer.valueOf(args[0]);
+    final int cport = Integer.valueOf(args[1]);
+    int timeout = Integer.valueOf(args[2]);
+    String fileFolder = args[3];
+     
+  }
+}
